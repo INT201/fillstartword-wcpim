4b699978b2f8ffd570d2230b0f552cfb06136b18
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-04 01:22:19 +0700

    fill

--- fillStartWord.js
+++ fillStartWord.js
@@ -4,3 +4,20 @@ function fillStartWord(startWord, word) {
   //code here
 }
 module.exports = fillStartWord
+
+function fillStartWord(startWord,word){
+  if(word === null || word === undefined){
+      return console.log(undefined)
+  }
+  else if(word.includes(startWord) == true){
+      return console.log(word)
+  }
+  else{
+      return console.log(startWord+word)
+  }
+}
+fillStartWord("1-2565-","Hello World")
+fillStartWord("JS","beginner")
+fillStartWord("first","firststep")
+fillStartWord("first",null)
+fillStartWord("first",undefined)
